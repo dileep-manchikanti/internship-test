ceea6bd54482d504256fe6749231c236569c5478
dileep <manchikantidileep@gmail.com>
2022-03-12 12:55:35 +0530

    internship

--- src/signIn/signIn.js
+++ src/signIn/signIn.js
@@ -9,11 +9,11 @@
             user.invalidEmail=true;
         }
         user.logIn=function(){
-            console.log(user.user.email);
-            console.log(user.user.password);
-            console.log(userService.user.email);
-            console.log(userService.user.password);
-            console.log(user.user.email===userService.user.email && user.user.password===userService.user.password);
+            // console.log(user.user.email);
+            // console.log(user.user.password);
+            // console.log(userService.user.email);
+            // console.log(userService.user.password);
+            // console.log(user.user.email===userService.user.email && user.user.password===userService.user.password);
             if(user.user.email===userService.user.email && user.user.password===userService.user.password){
                 userService.didSigned=true;
                 window.alert("logged in sucessfully...");
