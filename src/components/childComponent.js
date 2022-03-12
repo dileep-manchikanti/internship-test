e83b0e8b25bff687e12df8e5874593aabf6d1bde
dileep <manchikantidileep@gmail.com>
2022-03-12 22:33:00 +0530

    'internship'

--- src/components/childComponent.js
+++ src/components/childComponent.js
@@ -11,12 +11,13 @@
 
     function childComponentController(){
         var $ctrl=this;
-        console.log($ctrl.name);
-        if($ctrl.name!=""){
-            $ctrl.Name="Hi "+$ctrl.name;
-        }
-        else{
-            $ctrl.Name="";
-        }
+        // console.log($ctrl.name);
+        // if($ctrl.name!=""){
+        //     $ctrl.Name="Hi "+$ctrl.name;
+        // }
+        // else{
+        //     $ctrl.Name="";
+        // }
+        $ctrl.Name="Hi"+$ctrl.name;
     }
 })();
