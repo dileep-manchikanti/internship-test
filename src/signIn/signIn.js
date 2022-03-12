b0a11b1773d9f161c4501520038236a024d986e7
dileep <manchikantidileep@gmail.com>
2022-03-12 14:56:17 +0530

    internship'

--- src/signIn/signIn.js
+++ src/signIn/signIn.js
@@ -15,14 +15,12 @@
             user.invalidEmail=true;
         }
         user.logIn=function(){
-            console.log(user.user.email);
-            console.log(user.user.password);
-            console.log(service.user.email);
-            console.log(service.user.password);
-            console.log(user.user.email===service.user.email && user.user.password===service.user.password);
             if(user.user.email===service.user.email && user.user.password===service.user.password){
                 service.didSigned=true;
                 window.alert("logged in sucessfully...");
+            }
+            else{
+                window.alert('username or password in wrong.Please try Again!!')
             }
         }
     }
